e7836f3ec733799151d34644211139c7537925dc
Eldo <156140793+Eldonwhiteley@users.noreply.github.com>
2025-04-17 12:54:25 -0600

    Add files via upload

---
+++ lab11/lab11/binary.v
@@ -0,0 +1,64 @@
+// Implement binary state machine
+module binary(
+    input w,
+    input clk,
+    input reset,
+    output [2:0]State,
+    output z
+);
+
+wire [2:0] State;
+wire [2:0] Next;
+
+dff zero (
+    .reset(reset),
+    .Default(1'b0),
+    .D(Next[0]),
+    .clk(clk),
+    .Q(State[0])
+);
+
+dff one(
+    .reset(reset),
+    .Default(1'b0),
+    .D(Next[1]),
+    .clk(clk),
+    .Q(State[1])
+);
+
+dff two(
+    .reset(reset),
+    .Default(1'b0),
+    .D(Next[2]),
+    .clk(clk),
+    .Q(State[2])
+);
+
+assign z = (~State[2] & State[1] & ~State[0]) | (State[2] & ~State[1] & ~State[0]);
+
+assign Next[0] = 
+(~w & State[2] & ~State[1] & ~State[0]) |
+(~w & ~State[2] & State[1] & State[0]) |
+(~State[2] & ~State[1] & ~State[0]) | 
+(w & ~State[1] & State[0]) |
+(w & ~State[2] & ~State[0]);
+
+assign Next[1] =
+(w & ~State[2] & ~State[0]) | 
+(State[1] & ~State[0]) |
+(~State[1] & State[0]);
+
+
+assign Next[2] =
+(w & State[2]) |
+(w & State[1] & State[0]);
+
+
+
+//A = 000
+//B = 001
+//C = 010
+//D = 011
+//E = 100
+
+endmodule
---
+++ lab11/lab11/onehot.v
@@ -0,0 +1,59 @@
+// Implement one-hot state machine
+module onehot (
+    input w, clock, reset,
+    output A, B, C, D, E,
+    output z
+    );
+    
+    wire A_next, B_next, C_next, D_next, E_next;
+    wire A, B, C, D, E;
+      
+     dff Adff(
+        .Default(1'b1),
+        .D(A_next),
+        .clk(clock),
+        .Q(A),
+        .reset(reset)
+     );
+     
+     dff Bdff(
+        .Default(1'b0),
+        .D(B_next),
+        .clk(clock),
+        .Q(B),
+        .reset(reset)
+     );
+     
+     dff Cdff(
+        .Default(1'b0),
+        .D(C_next),
+        .clk(clock),
+        .Q(C),
+        .reset(reset)
+     );
+     
+     dff Ddff(
+        .Default(1'b0),
+        .D(D_next),
+        .clk(clock),
+        .Q(D),
+        .reset(reset)
+     );
+     
+     dff Edff(
+        .Default(1'b0),
+        .D(E_next),
+        .clk(clock),
+        .Q(E),
+        .reset(reset)
+     );
+    
+    assign z = C | E;
+    
+    assign A_next = 1'b0;
+    assign B_next = (A & ~w) | (D & ~w) | (E & ~w);
+    assign C_next = (B & ~w) | (C & ~w);
+    assign D_next = (A & w) | (B & w) | (C & w);
+    assign E_next = (D & w) | (E & w);
+    
+endmodule
